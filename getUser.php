74621f60992ce00daf6e197d0a43117c70998bea
Steven Enamakel <steven.enamakel@gmail.com>
2016-08-06 18:30:47 +0530

    added blah

--- getUser.php
+++ getUser.php
@@ -10,10 +10,36 @@ $q = "
     LIMIT 1;
 ";
 
+
 $result = $conn->query($q);
 
-if ($result->num_rows > 0) $resp = $result->fetch_assoc();
-else {
+if ($result->num_rows > 0) {
+    $resp = $result->fetch_assoc();
+    $userId = $resp["id"];
+    $q = "
+        SELECT * FROM relations
+            WHERE user_id='$userId'
+    ";
+
+    $resp["locationsIds"] = [];
+    $resp["jobRolesIds"] = [];
+    $resp["jobSectorsIds"] = [];
+    $resp["productChannelIds"] = [];
+    $resp["productNameIds"] = [];
+    $resp["serviceNamesIds"] = [];
+    $resp["serviceOccupationIds"] = [];
+
+    $result = $conn->query($q);
+    while ($data = $result->fetch_assoc()) {
+        if (isset($data["location_id"])) $resp["locationsIds"][] = $data["location_id"];
+        if (isset($data["job_role_id"])) $resp["jobRolesIds"][] = $data["job_role_id"];
+        if (isset($data["job_sector_id"])) $resp["jobSectorsIds"][] = $data["job_sector_id"];
+        if (isset($data["product_channel_id"])) $resp["productChannelIds"][] = $data["product_channel_id"];
+        if (isset($data["product_name_id"])) $resp["productNameIds"][] = $data["product_name_id"];
+        if (isset($data["service_name_id"])) $resp["serviceNamesIds"][] = $data["service_name_id"];
+        if (isset($data["service_occupation_id"])) $resp["serviceOccupationIds"][] = $data["service_occupation_id"];
+    }
+} else {
     $resp = array('message' => 'user_doesnt_exist');
     http_response_code(400);
 }
